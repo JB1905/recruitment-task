1b2b2cf64d564b98d8e486bc3889d5b70a8f2f73
Jakub Biesiada <biesiadajakub@icloud.com>
2026-03-31 22:03:20 +0200

    refactor: remove unused dependencies and dead code

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,8 +5,6 @@ const config = {
   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    // Redirect kittens constants (uses import.meta.env) to a CJS mock
-    '^../constants$': '<rootDir>/src/packages/kittens/__mocks__/constants.cjs',
   },
   transform: {
     '^.+\\.tsx?$': [
